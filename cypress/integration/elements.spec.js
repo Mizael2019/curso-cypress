88550822e068a3fac20eb16e39308b3e7e585a49
Mizael T. S. Nazareno <mtszareno@gmail.com>
2020-07-17 19:50:20 -0300

    Atualização estudos aula 14 e 15

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -9,12 +9,13 @@ describe('Trabalhando com elementos básicos', () => {
         cy.reload()
     })
 
+    //Aula 12
     it('Textos', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('contain', 'Cuidado')
 
-        //forma correta.
+        //forma correta, todo o texto.
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
 
@@ -28,12 +29,43 @@ describe('Trabalhando com elementos básicos', () => {
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
 
-    //Inicio aula 13
-    it.only('Campos de Textos (Textfields)', () => {
+    //Aula 13
+    it('Campos de Textos (Textfields)', () => {
         cy.get('#formNome').type('Cypress Teste')
+        cy.get('#formNome').should('have.value', 'Cypress Teste')
 
+        cy.get('#elementosForm\\:sugestoes')
+        .type('Teste de campo maior limpar')
+        .should('have.value', 'Teste de campo maior limpar')
 
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+        .type('Primeiro campo')
 
+        cy.get('[data-cy=dataSobrenome]')
+        .type('Teste12345{backspace}{backspace}', {delay: 100})
+        .should('have.value', 'Teste123')
 
+        cy.get('#elementosForm\\:sugestoes')
+        .clear()
+        .type('Erro{selectall}acerto', {delay: 100})
+        .should('have.value', 'acerto')
     })
+
+    //Aula 14
+    it('RadioButton', () => {
+        cy.get('#formSexoFem')
+        .click()
+        .should('be.checked')
+
+        cy.get('#formSexoMasc').should('not.be.checked')
+        cy.get("[name=formSexo]").should('have.length', 2)
+    })
+
+    //Aula 15
+    it.only('Checkbox', () => {
+        cy.get('#formComidaPizza')
+        .click()
+        .should('be.checked')
+    })
+
 })
